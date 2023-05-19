43271f2868e5604f772e8ac202e9ae06074487ab
Sefa Yılmaz <73781928+sefaa-dev@users.noreply.github.com>
2023-05-19 19:00:43 +0300

    plugin oluştup, admin tarafında install edildi.

---
+++ codeislife/codeislife.Widgets.SwiperSlider/SwiperSliderPlugin.cs
@@ -0,0 +1,43 @@
+﻿using Nop.Services.Plugins;
+
+namespace codeislife.Widgets.SwiperSlider
+{
+    public class SwiperSliderPlugin : BasePlugin
+    {
+        #region Fields
+
+        #endregion
+
+        #region Ctor
+        public SwiperSliderPlugin()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+        public override void PreparePluginToUninstall()
+        {
+            base.PreparePluginToUninstall();
+        }
+
+        //public override string GetConfigurationPageUrl()
+        //{
+        //    return base.GetConfigurationPageUrl();
+        //}
+        #endregion
+
+
+
+    }
+}
