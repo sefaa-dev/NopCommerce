94be6108ea794595dd26237cb093ff1d8e23c3bb
Sefa Yılmaz <73781928+sefaa-dev@users.noreply.github.com>
2023-05-19 17:22:44 +0300

    Add project files.

---
+++ codeislife/codeislife_firstplugin/FirstPlugin.cs
@@ -0,0 +1,11 @@
+﻿using Nop.Services.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeislife_firstplugin
+{
+    public class FirstPlugin : BasePlugin
+    {
+    }
+}
